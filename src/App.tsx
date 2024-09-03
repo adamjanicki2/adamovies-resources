0ad22872a5462d0cf7a66e7fba8e057e82d71a3a
Adam Janicki <adamjanicki2@gmail.com>
2024-09-02 21:21:19 -0700

    update for modern config

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from "react";
 import { ReactComponent as Logo } from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Logo className="App-logo" />
-        <a
-          className="App-link"
-          href="https://www.adamovies.com/login"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ marginTop: "48px" }}
-        >
-          Join Adamovies
-        </a>
-      </header>
+    <div className="container">
+      <Logo className="logo" />
+      <a
+        href="https://www.adamovies.com/login"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          marginTop: "4rem",
+          fontSize: "1.5rem",
+          fontWeight: 600,
+          color: "#ffa500",
+        }}
+      >
+        Join Adamovies
+      </a>
     </div>
   );
 }
